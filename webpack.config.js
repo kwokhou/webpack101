a59168a6ec5e311a6725033a861445cfa4e585dd
Andy Yong <kwokhou@gmail.com>
2017-06-11 12:49:58 +0800

    Added html-webpack-plugin to auto generate index.html
    - This package will automatically generate an
      index.html file which contain a script tag
      that loads the app.bundle.js
    
    - html-webpack-plugin will use the template index.ejs
      to generate the output index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: './dist/app.bundle.js'
-    }
-}
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "app.bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack 102",
+      template: "./src/index.ejs" // Load a custom template (ejs by default see the FAQ for details)
+    })
+  ]
+};
